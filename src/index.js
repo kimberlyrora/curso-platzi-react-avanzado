1cdc0706044bae8b348aa20952d32a7b4cb454e7
kimberlyrojasra <kimberlyrojasra@gmail.com>
2020-01-11 19:23:31 -0500

    passing prework to github

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import ApolloClient from 'apollo-boost'
+// import { ApolloProvider } from 'react-apollo'
+// import Context from './Context'
+// import { App } from './App'
+
+// const client =  new ApolloClient({
+// 	uri: 'https://petgram-server-kimislearning.now.sh/graphql'
+// })
+
+ReactDOM.render(
+	<h1>Hola</h1>
+	// <Context.Provider>
+	// 	<ApolloProvider client={client}>
+	// 		<App />
+	// 	</ApolloProvider>
+	// </Context.Provider>
+	, document.getElementById('app'))
